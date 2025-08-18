c785580e64ce25c82f1e4501fdecdf26411e682f
Vishwas Chaugule <chaugule1369vish@gmail.com>
2025-08-18 22:56:52 +0530

    add initial project code

---
+++ data.js
@@ -0,0 +1,16 @@
+module.exports = function () {
+    return {
+        products: [
+            { id: 1, name: "Kayak", catagory: "Watersports", description: "A boat for one person", price: 275},
+            { id: 2, name: "Lifejacket", catagory: "Watersports", description: "Protective and fashionable", price: 48.95},
+            { id: 3, name: "Soccer Ball", catagory: "Soccer", description: "FIFA-approved size and weight", price: 19.50},
+            { id: 4, name: "Corner Flags", catagory: "Soccer", description: "Give your playing field a professional touch", price: 34.95},
+            { id: 5, name: "Stadium", catagory: "Soccer", description: "Flat-packed 35,000-seat stadium", price: 79500},
+            { id: 6, name: "Thinking Cap", catagory: "Chess", description: "Improve brain efficiency by 75%", price: 16},
+            { id: 7, name: "Unsteady Chair", catagory: "Chess", description: "Secretly give your opponent a disadvantage", price: 29.95},
+            { id: 8, name: "Human Chess Board", catagory: "Chess", description: "A fun game for the family", price: 75},
+            { id: 9, name: "Bling King", catagory: "Chess", description: "Gold-plated, diamond-studed King", price: 1200}
+        ],
+        orders: []
+    }
+}
